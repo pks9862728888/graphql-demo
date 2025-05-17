dc0f3342122981b1c9060a231c6dacb5d651226d
pks9862728888 <pks9862728888@gmail.com>
2025-05-17 13:06:31 +0530

    Added navbar

---
+++ frontend/src/app/_components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+      <h3 className={styles.title}>Cloudmart</h3>
+    </div>
+  );
+}
+
+export default Navbar;
